40754904f176c0d96e007f537bd0defcdaedc667
MalcolmSmalls <smallsbeats@gmail.com>
2022-10-10 12:05:01 -0700

    Add other tests

--- src/Ships.test.js
+++ src/Ships.test.js
@@ -4,5 +4,13 @@ import { Ships } from "./Ships.js"
 const carrier = Ships('carrier')
 
 test('OY mate! Let\'s build some ships', () => {
+    expect(carrier.sunk()).toEqual(false)
     expect(carrier.getType()).toEqual('carrier')
+    expect(carrier.shipSize).toEqual(5)
+    carrier.hit()
+    carrier.hit()
+    carrier.hit()
+    carrier.hit()
+    expect(carrier.hit()).toEqual(0)
+    expect(carrier.sunk()).toEqual(true)
 })
